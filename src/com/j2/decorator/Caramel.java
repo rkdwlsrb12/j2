7d963c93503a5c6e65e11009fa1527c19ccdd381
rkdwlsrb12 <sooshin12@naver.com>
2015-10-08 19:57:51 +0900

    decorator

---
+++ src/com/j2/decorator/Caramel.java
@@ -0,0 +1,17 @@
+package com.j2.decorator;
+public class Caramel extends CondimentDecorator{
+	 
+	public Caramel(Beverage beverage) {
+		this.beverage = beverage;
+	}
+ 
+	public String getDescription() {
+		return beverage.getDescription() + ", Caramel";
+	}
+ 
+	public double cost() {
+		return .80 + beverage.cost();
+	}
+
+
+}
